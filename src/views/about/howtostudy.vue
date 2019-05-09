472afd389715f737a404ddbb7e8e940e5ccda00b
CoderTonyChan <101992315@qq.com>
2019-05-09 16:41:52 +0800

    fix 如何上课

--- src/views/about/howtostudy.vue
+++ src/views/about/howtostudy.vue
@@ -14,28 +14,23 @@
         <h1 style="text-align: center;">如何上课</h1>
         <ul style="font-size: 20px;">
               <li>一、学员请在老师好在线（www.lshao.cn）注册账号</li>
-              <li>二、下载老师好App，并在客户端下载界面认真观看《老师好操作指南》视频</li>
-              <li>三、试用老师好App
-                <br><span style="margin-left: 20px;">操作步骤：</span>
-                <br><span style="margin-left: 20px;">1、打开老师好App，并用注册账号登录，界面显示大致如下：</span>
+              <li>二、网上报班并在线支付：请记住您选择的培训机构名称，第一次登录老师好App时需要勾选</li>
+              <li>三、下载老师好App，并在客户端下载界面认真观看《学生如何上课》和《如何写作业》这两个视频</li>
+              <li>四、用老师好App的步骤：
+                <br><span style="margin-left: 20px;">1、打开老师好App，选择培训机构，并用学员的注册账号登录，界面显示类似如下：</span>
                 <br><img style="margin-left: 20px;" src="https://user-images.githubusercontent.com/16996753/54809765-339e7d00-4cbf-11e9-8f29-e5335c444a4c.png" alt="">
-                <br><span style="margin-left: 20px;">2、点击班级课程，界面显示如下：</span>
+                <br><span style="margin-left: 20px;">2、点击班级课程，界面显示类似如下：</span>
                 <br><img style="margin-left: 20px;" src="https://user-images.githubusercontent.com/16996753/54809772-3b5e2180-4cbf-11e9-9894-6d8c3bd6333c.png" alt="">
                 <br><span style="margin-left: 20px;">3、点击观看视频课</span>
-                <br><span style="margin-left: 20px;">4、手写课后作业：点击作业->点击书写作业，界面显示如下：</span>
+                <br><span style="margin-left: 20px;">4、手写课后作业：点击作业->点击书写作业，界面显示类似如下：</span>
                 <br><img style="margin-left: 20px;" src="https://user-images.githubusercontent.com/16996753/54809775-3dc07b80-4cbf-11e9-9fe7-9c2c0b38f828.png" alt="">
                 <br><span style="margin-left: 20px;">请熟悉使用各种白板功能来书写作业，尤其是随笔和板书两大功能。强力推荐使用Pad和电容笔书写作业，如果您的Pad支持使用Apple pencil 那样精度高的电容笔，请用随笔功能写作业，感觉就像在纸上写作业；如果您使用的是普通电容笔，建议用板书功能，可以全屏手写，不必费眼死盯着屏幕，手写的大字会自动缩小进入文本框，并可在文本框内逐字编辑手写字；如果您没有Pad，也可在纸上写完作业后，通过手机拍照插入作业（图片）。友情提示：可先由板书定位光标，然后再打开“选项”菜单，选择“插入图片”，则图片左上角与光标左上角自动对齐</span>
 
-                <br><span style="margin-left: 20px;">5、正式使用时，还需要提交作业请老师批改，然后，再查看作业或考试成绩，自己比对标准答案修改错题</span>
+                <br><span style="margin-left: 20px;">5、提交作业请老师批改</span>
+                <br><span style="margin-left: 20px;">6、查看作业或考试成绩，使用双指左右滑动的手势操作或鼠标左键双击，可以自己对照标准答案修改错题</span>
               </li>
               <li>四、网上报班：请记住您选择的培训机构名称，第一次登录老师好App时需要勾选</li>
-              <li>五、开始正式使用老师好App上课
-                <br><span style="margin-left: 20px;">操作步骤：</span>
-                <br><span style="margin-left: 20px;">1、选择培训机构</span>
-                <br><span style="margin-left: 20px;">2、学员登录</span>
-                <br><span style="margin-left: 20px;">3、参见上面三、所述，按老师的班级课程（表）观看视频课、在线写课后作业，学完课程后由老师组织进行在线结业考试，给家长汇报结业成绩</span>
-              </li>
-              <li>六、师生及时沟通：学员以手机号注册，请老师组织学员，可在老师好App的班级群里互发消息进行沟通，也可建立QQ或微信群进行沟通</li>
+              <li>五、师生及时沟通：学员以手机号注册，请老师组织学员，可在老师好App的班级群里互发消息进行沟通，也可建立QQ或微信群进行沟通</li>
             </ul>
       </div>
     </div>
